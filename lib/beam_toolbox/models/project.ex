fc55472d4622abc51843cdf1b6bd3210af77b881
Josh Adams <josh@isotope11.com>
2014-03-12 23:11:39 -0500

    Implement the cached version of the statistics module

--- lib/beam_toolbox/models/project.ex
+++ lib/beam_toolbox/models/project.ex
@@ -17,6 +17,12 @@ defmodule BeamToolbox.Models.Project do
 
   def readme_raw(project), do: GitHub.Raw.readme(project.github)
   def readme(project), do: project |> readme_raw |> Markdown.to_html(tables: true, fenced_code: true, autolink: true)
+  def readme_caching(project) do
+    {:ok, value} = :cadfaerl.get_or_fetch_ttl(:github, :"readme#{project.github}", fn() ->
+                     readme(project)
+                   end, 600)
+    value
+  end
 
   defmodule Statistics do
     import Enum
@@ -36,6 +42,21 @@ defmodule BeamToolbox.Models.Project do
     def description(repo_ident) do
       repo = repo_ident |> GitHub.repo
       repo["description"]
+    end
+
+    defmodule Cached do
+      @time_to_live 600 # 10 minutes
+
+      [:stargazers_count, :forks_count, :latest_commit_date, :description]
+      |> Enum.each fn(fun_name) ->
+        def unquote(fun_name)(repo_ident) do
+          fun_name = unquote(fun_name)
+          {:ok, value} = :cadfaerl.get_or_fetch_ttl(:github, :"#{fun_name}#{repo_ident}", fn() ->
+                           apply(BeamToolbox.Models.Project.Statistics, fun_name, [repo_ident])
+                         end, @time_to_live)
+          value
+        end
+      end
     end
   end
 end
